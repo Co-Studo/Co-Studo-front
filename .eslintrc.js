611c61924ab8a07a562b4643a477f81b7c1d3e17
healtheloper <58503584+healtheloper@users.noreply.github.com>
2022-08-04 17:47:30 +0900

    :wrench: eslint config update

--- .eslintrc.js
+++ .eslintrc.js
@@ -19,6 +19,8 @@ module.exports = {
     project: './tsconfig.json',
   },
   rules: {
+    'import/no-extraneous-dependencies': 'off',
+    'react/jsx-props-no-spreading': 'off',
     'react/prop-types': 'off',
     // only use arrow-function component
     // https://github.com/jsx-eslint/eslint-plugin-react/blob/master/docs/rules/function-component-definition.md
