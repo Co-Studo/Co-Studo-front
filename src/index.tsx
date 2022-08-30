5f19474a3b9e36cd90b323ec5858877b8c5e9d9e
healtheloper <58503584+healtheloper@users.noreply.github.com>
2022-08-30 12:27:21 +0900

    :sparkles: msw setting

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,8 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import ReactDOM from 'react-dom/client';
 import { RecoilRoot } from 'recoil';
 
+import { serviceWorker } from '@mocks/browser';
+
 import App from './App';
 
 const queryClient = new QueryClient();
@@ -9,6 +11,10 @@ const queryClient = new QueryClient();
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement,
 );
+
+if (process.env.NODE_ENV === 'development') {
+  serviceWorker.start();
+}
 
 root.render(
   <RecoilRoot>
