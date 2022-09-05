a1006319d46876dd63d9fcf2e89541c90e6eff74
healtheloper <58503584+healtheloper@users.noreply.github.com>
2022-09-05 16:55:38 +0900

    :arrow_up: storybook dep update

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,22 +1,16 @@
 module.exports = {
   env: {
-    browser: true,
+    browser: true
   },
   settings: {
     'import/resolver': {
-      typescript: {},
-    },
+      typescript: {}
+    }
   },
-  extends: [
-    'airbnb',
-    'airbnb-typescript',
-    'airbnb/hooks',
-    'prettier',
-    'plugin:@typescript-eslint/recommended',
-  ],
+  extends: ['airbnb', 'airbnb-typescript', 'airbnb/hooks', 'prettier', 'plugin:@typescript-eslint/recommended', 'plugin:storybook/recommended'],
   parser: '@typescript-eslint/parser',
   parserOptions: {
-    project: './tsconfig.json',
+    project: './tsconfig.json'
   },
   rules: {
     // ---------------------------------------
@@ -24,35 +18,30 @@ module.exports = {
     'jsx-a11y/no-static-element-interactions': 'off',
     'jsx-a11y/click-events-have-key-events': 'off',
     // ---------------------------------------
-    'no-console': ['off', { allow: ['error'] }],
+    'no-console': ['off', {
+      allow: ['error']
+    }],
     'import/prefer-default-export': 'off',
     'import/no-extraneous-dependencies': 'off',
     'react-hooks/rules-of-hooks': 'error',
-    'react-hooks/exhaustive-deps': [
-      'warn',
-      {
-        additionalHooks: 'useRecoilCallback',
-      },
-    ],
+    'react-hooks/exhaustive-deps': ['warn', {
+      additionalHooks: 'useRecoilCallback'
+    }],
     'react/jsx-props-no-spreading': 'off',
     'react/prop-types': 'off',
     // only use arrow-function component
     // https://github.com/jsx-eslint/eslint-plugin-react/blob/master/docs/rules/function-component-definition.md
-    'react/function-component-definition': [
-      2,
-      { namedComponents: 'arrow-function' },
-    ],
+    'react/function-component-definition': [2, {
+      namedComponents: 'arrow-function'
+    }],
     'react/react-in-jsx-scope': 'off',
-    'import/order': [
-      'error',
-      {
-        groups: ['builtin', 'external', 'internal', 'index'],
-        alphabetize: {
-          order: 'asc',
-          caseInsensitive: true,
-        },
-        'newlines-between': 'always',
+    'import/order': ['error', {
+      groups: ['builtin', 'external', 'internal', 'index'],
+      alphabetize: {
+        order: 'asc',
+        caseInsensitive: true
       },
-    ],
-  },
+      'newlines-between': 'always'
+    }]
+  }
 };
