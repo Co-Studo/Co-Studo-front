b6679cc8c2b3da452d8e00180761b12a9cf51c4b
healtheloper <58503584+healtheloper@users.noreply.github.com>
2022-09-08 16:36:13 +0900

    :lipstick: add paper component

--- src/components/common/Paper/index.tsx
+++ src/components/common/Paper/index.tsx
@@ -1,16 +1,20 @@
 import { ReactNode } from 'react';
-import { css } from 'styled-components';
 
 import colors from '@theme/colors';
 
-const Paper: React.FC<{ children: ReactNode }> = ({ children }) => (
+const Paper: React.FC<{ children: ReactNode; sx?: object }> = ({
+  children,
+  sx,
+}) => (
   <div
-    css={css`
-      padding: 1rem;
-      border-radius: 0.5rem;
-      box-shadow: 0px 4px 10px ${colors.greyOpacity100},
-        0px 0px 4px ${colors.greyOpacity500};
-    `}
+    css={{
+      width: 'max-content',
+      padding: '1rem',
+      borderRadius: '0.5rem',
+      boxShadow: `0px 4px 10px ${colors.greyOpacity100},
+        0px 0px 4px ${colors.greyOpacity500}`,
+      ...sx,
+    }}
   >
     {children}
   </div>
