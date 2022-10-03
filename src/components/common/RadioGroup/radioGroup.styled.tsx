fbf26786c05cd4d8d224b35e6af36024b937013f
Hemudi <ksum1205@naver.com>
2022-10-03 22:55:50 +0900

    :lipstick: update style of StyledRadioGroup

--- src/components/common/RadioGroup/radioGroup.styled.tsx
+++ src/components/common/RadioGroup/radioGroup.styled.tsx
@@ -1,33 +1,55 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 
 import RadioGroup from '@components/common/RadioGroup';
 
 const StyledRadioGroup = styled(RadioGroup)`
-  min-width: 580px;
-`;
-
-const StyleRadioTitle = styled(RadioGroup.Title)`
-  padding-bottom: 10px;
-  font-size: 13px;
-`;
-
-const StyledOptionWrapper = styled.div`
   display: flex;
-  gap: 20px;
+  flex-direction: row;
+  width: 355px;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+const CustomRadioButtonStyle = css`
+  -webkit-appearance: none;
+  appearance: none;
+  width: 32px;
+  height: 32px;
+  margin: 0;
+  padding: 6px;
+  border: 2px solid #0000007f;
+  border-radius: 50%;
+  background-clip: content-box;
+
+  :checked {
+    background-color: #007aff;
+    :hover {
+      background-color: #007aff;
+    }
+  }
+
+  :hover {
+    background-color: #85c0fe;
+  }
 `;
 
 const StyledRadioOption = styled(RadioGroup.Option)`
   display: flex;
   align-items: center;
-  gap: 5px;
-  font-size: 12px;
+
+  input[type='radio'] {
+    ${CustomRadioButtonStyle}
+  }
+
+  label {
+    font-size: 17px;
+    padding: 10px;
+  }
 `;
 
 const StyledRadio = {
   Group: StyledRadioGroup,
-  Title: StyleRadioTitle,
   Option: StyledRadioOption,
-  OptionWrapper: StyledOptionWrapper,
 };
 
 export default StyledRadio;
