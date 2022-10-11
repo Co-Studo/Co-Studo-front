6d07451f005342b795a7639f237209ddd2a76b44
mina-gwak <mina.gwak@kakao.com>
2022-10-12 04:47:24 +0900

    :recycle: change editor to uncontrolled component

--- src/components/common/TextEditor/index.tsx
+++ src/components/common/TextEditor/index.tsx
@@ -11,13 +11,15 @@ import tableMergedCell from '@toast-ui/editor-plugin-table-merged-cell';
 import { Editor as ToastEditor } from '@toast-ui/react-editor';
 import prism from 'prismjs';
 import * as React from 'react';
-import { Dispatch, SetStateAction, useEffect } from 'react';
+import { forwardRef, useEffect, useImperativeHandle } from 'react';
 
 import { getImageUrl } from '@apis/image';
+import colors from '@theme/colors';
 
-type TextEditorOptions = {
+export type TextEditorOptions = {
   width?: string;
   height?: string;
+  initialValue?: string;
   initialEditType?: 'markdown' | 'wysiwyg';
   previewStyle?: 'tab' | 'vertical';
   autofocus?: boolean;
@@ -27,65 +29,67 @@ type TextEditorOptions = {
 const defaultTextEditorOptions: TextEditorOptions = {
   width: '800px',
   height: '300px',
+  initialValue: ' ',
   initialEditType: 'wysiwyg',
   previewStyle: 'vertical',
   autofocus: true,
   placeholder: '',
 };
 
-type TextEditorProps = {
-  htmlStr: string;
-  setHtmlStr: Dispatch<SetStateAction<string>>;
+export type TextEditorProps = {
   options?: TextEditorOptions;
 };
 
-const TextEditor = ({
-  htmlStr,
-  setHtmlStr,
-  options: { width, ...restOptions } = defaultTextEditorOptions,
-}: TextEditorProps) => {
-  const editorRef = React.useRef<ToastEditor>(null);
-
-  const handleEditorChange = () => {
-    if (editorRef.current) {
-      setHtmlStr(editorRef.current.getInstance().getHTML());
-    }
-  };
-
-  useEffect(() => {
-    if (editorRef.current) {
-      editorRef.current.getInstance().removeHook('addImageBlobHook');
-      editorRef.current
-        .getInstance()
-        .addHook('addImageBlobHook', (blob, callback) => {
-          (async () => {
-            const {
-              results: { imageUrl },
-            } = await getImageUrl(blob);
-            callback(imageUrl, blob.name);
-          })();
-          return false;
-        });
-    }
-  }, []);
-
-  return (
-    <div css={{ backgroundColor: '#fff', width, borderRadius: '4px' }}>
-      <ToastEditor
-        ref={editorRef}
-        useCommandShortcut
-        initialValue={htmlStr}
-        minHeight="300px"
-        onChange={handleEditorChange}
-        plugins={[
-          colorSyntax,
-          tableMergedCell,
-          [codeSyntaxHighlight, { prism }],
-        ]}
-        {...restOptions}
-      />
-    </div>
-  );
+export type TextEditorHandle = {
+  getHTML: () => string;
+  getMarkdown: () => string;
 };
+
+const TextEditor = forwardRef<TextEditorHandle, TextEditorProps>(
+  ({ options: { width, ...restOptions } = defaultTextEditorOptions }, ref) => {
+    const editorRef = React.useRef<ToastEditor>(null);
+
+    useImperativeHandle(ref, () => ({
+      getHTML: () =>
+        (editorRef.current && editorRef.current.getInstance().getHTML()) || '',
+      getMarkdown: () =>
+        (editorRef.current && editorRef.current.getInstance().getMarkdown()) ||
+        '',
+    }));
+
+    useEffect(() => {
+      if (editorRef.current) {
+        editorRef.current.getInstance().removeHook('addImageBlobHook');
+        editorRef.current
+          .getInstance()
+          .addHook('addImageBlobHook', (blob, callback) => {
+            (async () => {
+              const {
+                results: { imageUrl },
+              } = await getImageUrl(blob);
+              callback(imageUrl, blob.name);
+            })();
+            return false;
+          });
+      }
+    }, []);
+
+    return (
+      <div css={{ backgroundColor: colors.white, width, borderRadius: '4px' }}>
+        <ToastEditor
+          ref={editorRef}
+          useCommandShortcut
+          minHeight="300px"
+          plugins={[
+            colorSyntax,
+            tableMergedCell,
+            [codeSyntaxHighlight, { prism }],
+          ]}
+          {...restOptions}
+        />
+      </div>
+    );
+  },
+);
 
 export default TextEditor;
--- src/components/common/TextEditor/textEditor.stories.tsx
+++ src/components/common/TextEditor/textEditor.stories.tsx
@@ -1,7 +1,8 @@
 import { ComponentStory, ComponentMeta } from '@storybook/react';
-import { useState } from 'react';
+import * as React from 'react';
 
-import TextEditor from '@components/common/TextEditor';
+import Button from '@components/common/Button/button.styled';
+import TextEditor, { TextEditorHandle } from '@components/common/TextEditor';
 
 export default {
   title: 'common/TextEditor',
@@ -9,6 +10,28 @@ export default {
 } as ComponentMeta<typeof TextEditor>;
 
 export const Default: ComponentStory<typeof TextEditor> = () => {
-  const [htmlStr, setHtmlStr] = useState(' ');
-  return <TextEditor htmlStr={htmlStr} setHtmlStr={setHtmlStr} />;
+  const editorRef = React.useRef<TextEditorHandle>(null);
+
+  const handleButtonClick = () => {
+    if (editorRef.current) {
+      const text = editorRef.current.getHTML();
+      console.log(text);
+      // 이후 동작 ...
+    }
+  };
+
+  return (
+    <div
+      css={{
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'flex-end',
+        gap: '8px',
+        width: 'fit-content',
+      }}
+    >
+      <TextEditor ref={editorRef} />
+      <Button onClick={handleButtonClick}>등록하기</Button>
+    </div>
+  );
 };
