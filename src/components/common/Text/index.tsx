6f9088bb5a1b9577b49ea45abe1d67c664b47a78
jindonyy <rsefa94@gmail.com>
2022-10-14 07:46:53 +0900

    :sparkles: feat Text component.

---
+++ src/components/common/Text/index.tsx
@@ -0,0 +1,47 @@
+import { ReactElement } from 'react';
+import { useTheme } from 'styled-components';
+
+import fonts from '@theme/fonts';
+import { IPalette } from '@theme/theme';
+import { typography } from '@theme/typography';
+
+type TextSX = {
+  fontSize?: keyof typeof fonts.fontSize;
+  fontWeight?: keyof typeof fonts.fontWeight;
+  fontFamily?: keyof typeof fonts.fontFamily;
+  fontStyle?: keyof typeof fonts.fontStyle;
+  color?: keyof IPalette;
+  lineHeight?: keyof typeof fonts.lineHeight;
+  letterSpacing?: keyof typeof fonts.letterSpacing;
+};
+
+type TextProps = {
+  variant?: keyof typeof typography;
+  sx?: TextSX;
+  render: (style) => ReactElement;
+};
+
+const Text = ({ variant, sx, render }: TextProps) => {
+  const theme = useTheme();
+  const getVariantStyle = () => variant && typography[variant];
+  const getCustomStyle = () => {
+    const customStyle = { ...sx };
+
+    Object.keys(customStyle).forEach((key) => {
+      const value = customStyle[key];
+
+      customStyle[key] =
+        key === 'color' ? theme.palette[value] : fonts[key][value];
+    });
+
+    return customStyle;
+  };
+  const textStyle = {
+    ...getVariantStyle(),
+    ...getCustomStyle(),
+  };
+
+  return render(textStyle);
+};
+
+export default Text;
