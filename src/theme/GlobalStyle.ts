d54ecf89e3fbb57c23dbac26117ba01397f88ef5
jindonyy <rsefa94@gmail.com>
2022-10-14 07:45:28 +0900

    :lipstick: add body css in GlobalStyle.

--- src/theme/GlobalStyle.ts
+++ src/theme/GlobalStyle.ts
@@ -1,59 +1,65 @@
 import { createGlobalStyle } from 'styled-components';
 import reset from 'styled-reset';
 
+import fonts from '@theme/fonts';
+
+import { typography } from './typography';
+
 const GlobalStyle = createGlobalStyle`
   ${reset}
-  html {
-    font-family: Roboto, NotoSansKR;
-  }
-  *{
+
+  * {
     box-sizing: border-box;
   }
-  button{
-    margin:0;
-    padding:0;
-    border:none;
-    background-color:inherit;
-    border-radius: inherit;
-    cursor:pointer;
+
+  html {
+    font-size: 62.5%;
   }
-  input{
-    border:none;
-    border-radius:inherit;
-    margin:0;
-    padding:0;
-    :focus-visible{
-      outline:none;
-    }
-  }
-  input[type="file"] {
-    position: absolute;
-    width: 0;
-    height: 0;
-    padding: 0;
-    overflow: hidden;
-    border: 0;
-  }
-  textarea{
-    margin:0;
-    padding:0;
-    border:none;
-    height:auto;
-    max-width:100%;
-    background-color:inherit;
-    border-radius:inherit;
-    color:inherit;
-    :focus-visible{
-      outline:none;
-    }
-  }
-  a{
-    color:inherit;
-    text-decoration:none;
-  }
-  body{
+  
+  body {
     background-color: ${({ theme }) => theme.palette.bgColor};
+    ${typography.body}
     color: ${({ theme }) => theme.palette.fontColor};
+    letter-spacing: ${fonts.letterSpacing.base};
+  }
+
+  button {
+    margin: 0;
+    padding: 0;
+    border: none;
+    background-color: inherit;
+    border-radius: inherit;
+    cursor: pointer;
+  }
+
+  input {
+    margin: 0;
+    padding: 0;
+    border: none;
+    border-radius: inherit;
+    :focus-visible{
+      outline: none;
+    }
+  }
+
+  textarea {
+    max-width: 100%;
+    height: auto;
+    margin: 0;
+    padding: 0;
+    border: none;
+    background-color: inherit;
+    border-radius: inherit;
+    color: inherit;
+    :focus-visible{
+      outline:none;
+    }
+  }
+
+  a {
+    font: inherit;
+    color: inherit;
+    text-decoration: none;
   }
 `;
 
