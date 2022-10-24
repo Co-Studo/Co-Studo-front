4b5b04f0406321dc70dc487f7e82d168cbd4e5ad
jindonyy <rsefa94@gmail.com>
2022-10-25 04:36:49 +0900

    :recycle: add try..catch to validate function.

--- src/utils/validation.ts
+++ src/utils/validation.ts
@@ -2,19 +2,36 @@ import { all } from '@utils/functional';
 
 const isInteger = (number: number) => {
   const stringNumber = number.toString();
-  return number === parseInt(stringNumber, 10);
+  const ok = number === parseInt(stringNumber, 10);
+
+  if (!ok) throw Error(`${number}is not integer.`);
+
+  return ok;
 };
 
-const isNaturalNumber = (number: number) => isInteger(number) && number > 0;
+const isNaturalNumber = (number: number) => {
+  const ok = isInteger(number) && number > 0;
 
-type Values<V> = { [key in string]: V };
+  if (!ok) throw Error(`${number}is not integer.`);
+
+  return ok;
+};
 
 type ValidationFunction<V> = (value: V) => boolean;
 
-const validate = <V>(values: Values<V>, funcs: ValidationFunction<V>[]) => {
-  Object.entries(values).forEach(([key, value]) => {
-    if (!all(...funcs)(value)) throw new Error(`Please check ${key}.`);
-  });
+const validate = <V>(value: V, ...funcs: ValidationFunction<V>[]) => {
+  try {
+    const ok = all(...funcs)(value);
+
+    return { ok };
+  } catch (error) {
+    const message = error instanceof Error ? error.message : String(error);
+
+    return {
+      ok: false,
+      message,
+    };
+  }
 };
 
 export { isInteger, isNaturalNumber, validate };
