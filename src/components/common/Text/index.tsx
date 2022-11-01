6dd8b2cf12dc3a9a1ff7c0f796a4b038f6904d92
jindonyy <rsefa94@gmail.com>
2022-11-02 01:01:06 +0900

    :recycle: refact from getAs function to element object.

--- src/components/common/Text/index.tsx
+++ src/components/common/Text/index.tsx
@@ -48,6 +48,14 @@ const StyledText = styled.span<StyledProp>`
     }};
 `;
 
+const element = {
+  logo: 'h1',
+  sectionTitle: 'h2',
+  sectionDescription: 'p',
+  articleTitle: 'h3',
+  articleDescription: 'p',
+};
+
 const Text = ({ variant, sx = {}, as, ellipsis, children }: TextProps) => {
   const theme = useTheme();
   const getVariantStyle = () => variant && typography[variant];
@@ -55,23 +63,13 @@ const Text = ({ variant, sx = {}, as, ellipsis, children }: TextProps) => {
     ...getVariantStyle(),
     ...getCustomStyle(sx, theme),
   };
-  const getAs = () => {
-    switch (variant) {
-      case 'logo':
-        return 'h1';
-      case 'sectionTitle':
-        return 'h2';
-      case 'sectionDescription' || 'articleDescription':
-        return 'p';
-      case 'articleTitle':
-        return 'h3';
-      default:
-        return as;
-    }
-  };
 
   return (
-    <StyledText as={as || getAs()} fontCss={fontCss} ellipsis={ellipsis}>
+    <StyledText
+      as={as || (variant && element[variant])}
+      fontCss={fontCss}
+      ellipsis={ellipsis}
+    >
       {children}
     </StyledText>
   );
