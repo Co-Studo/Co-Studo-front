0aef14e1a9a694735fdf3c664a5d0b64e66a1ad5
jindonyy <rsefa94@gmail.com>
2022-11-04 05:03:55 +0900

    :recycle: separate iconSizes according to icon size.
    each icon has a diffrent size.

--- src/components/common/Icon/index.tsx
+++ src/components/common/Icon/index.tsx
@@ -5,21 +5,36 @@ import { Palette } from '@styles/theme';
 
 type IconProps = {
   iconName: IconName;
-  size?: 'small' | 'medium' | 'large';
+  size?: 'small' | 'medium' | 'large' | 'xLarge';
   color?: keyof Palette;
 };
 
 const iconSizes = {
-  small: '0.5rem',
-  medium: '1rem',
-  large: '2rem',
+  anonymous: {
+    small: '3.2rem',
+    medium: '4.2rem',
+    large: '5rem',
+    xLarge: '14.2rem',
+  },
+  bell: {
+    medium: '3.2rem',
+  },
+  crown: {
+    medium: '2.1rem',
+  },
+  lock: {
+    medium: '1.3rem',
+  },
+  star: {
+    medium: '2.5rem',
+  },
 };
 
-const Icon = ({ iconName, size = 'small', color }: IconProps) => {
+const Icon = ({ iconName, size = 'medium', color }: IconProps) => {
   const IconComponent = icon[iconName];
 
   const StyledIcon = styled(IconComponent)`
-    width: ${iconSizes[size]};
+    width: ${iconSizes[iconName][size] || iconSizes[iconName].medium};
     height: auto;
     fill: ${({ theme }) => theme.palette[color || 'primary']};
     stroke: ${({ theme }) => theme.palette[color || 'primary']};
