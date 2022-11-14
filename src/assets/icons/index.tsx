f3d4533eb99d06395758103dfc8dd699fed6ac79
jindonyy <rsefa94@gmail.com>
2022-11-15 02:27:07 +0900

    :fire: remove anonymous icon.

--- src/assets/icons/index.tsx
+++ src/assets/icons/index.tsx
@@ -1,11 +1,9 @@
-import Anonymous from '@assets/icons/anonymous.svg';
 import Bell from '@assets/icons/bell.svg';
 import Crown from '@assets/icons/crown.svg';
 import Lock from '@assets/icons/lock.svg';
 import Star from '@assets/icons/star.svg';
 
 export const icon = {
-  anonymous: Anonymous,
   bell: Bell,
   crown: Crown,
   lock: Lock,
