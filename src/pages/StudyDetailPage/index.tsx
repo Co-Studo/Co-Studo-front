f1847844fbd08f9b61a9005aecfd08cffb2964d5
mina-gwak <mina.gwak@kakao.com>
2022-12-09 01:51:10 +0900

    :lipstick: Edit style and markup

--- src/pages/StudyDetailPage/index.tsx
+++ src/pages/StudyDetailPage/index.tsx
@@ -61,7 +61,9 @@ const StudyDetailPage = () => {
           <img
             src="https://user-images.githubusercontent.com/62706988/204448063-766330af-a195-4985-b875-ec2950d2fa23.svg"
             alt={title}
-            width="142px"
+            css={{
+              width: '14.2rem',
+            }}
           />
           <div>
             <Text as="h2" variant="articleTitle" sx={{ mt: '0.6rem', mb: 1 }}>
@@ -97,38 +99,46 @@ const StudyDetailPage = () => {
           </div>
         </FlexBox>
         <FlexBox sx={{ alignItems: 'center', gap: '5rem' }}>
-          <FlexBox sx={{ flexShrink: 0, gap: '3rem' }}>
+          <FlexBox sx={{ flexShrink: 0, gap: '3rem' }} as="dl">
             <div css={{ textAlign: 'center' }}>
-              <Text
-                as="h3"
-                sx={{ mb: 1, fontSize: 'small', fontWeight: 'bold' }}
-              >
-                스터디 리더
-              </Text>
-              <Avatar
-                src={owner.photoURL}
-                alt={owner.displayName}
-                sx={{ width: '4rem', height: '4rem' }}
-              />
+              <dt>
+                <Text
+                  as="span"
+                  sx={{ mb: 1, fontSize: 'small', fontWeight: 'bold' }}
+                >
+                  스터디 리더
+                </Text>
+              </dt>
+              <dd>
+                <Avatar
+                  src={owner.photoURL}
+                  alt={owner.displayName}
+                  sx={{ width: '4rem', height: '4rem' }}
+                />
+              </dd>
             </div>
             {participants.length !== 0 && (
               <div css={{ textAlign: 'center' }}>
-                <Text
-                  as="h3"
-                  sx={{ mb: 1, fontSize: 'small', fontWeight: 'bold' }}
-                >
-                  스터디 멤버
-                </Text>
-                <AvatarGroup flexAlign="flex-end" max={3} spacing="small">
-                  {participants.map((member) => (
-                    <Avatar
-                      key={member.id}
-                      src={member.photoURL}
-                      alt={member.displayName}
-                      sx={{ width: '4.2rem', height: '4.2rem' }}
-                    />
-                  ))}
-                </AvatarGroup>
+                <dt>
+                  <Text
+                    as="span"
+                    sx={{ mb: 1, fontSize: 'small', fontWeight: 'bold' }}
+                  >
+                    스터디 멤버
+                  </Text>
+                </dt>
+                <dd>
+                  <AvatarGroup flexAlign="flex-end" max={3} spacing="small">
+                    {participants.map((member) => (
+                      <Avatar
+                        key={member.id}
+                        src={member.photoURL}
+                        alt={member.displayName}
+                        sx={{ width: '4.2rem', height: '4.2rem' }}
+                      />
+                    ))}
+                  </AvatarGroup>
+                </dd>
               </div>
             )}
           </FlexBox>
