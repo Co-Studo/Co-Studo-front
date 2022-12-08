4a60b104ce62d63a4a1fb8ca797c0952418b3a1d
Hemudi <ksum1205@naver.com>
2022-12-09 04:41:27 +0900

    :white_check_mark: update PageLayout story

--- src/components/common/PageLayout/pageLayout.stories.tsx
+++ src/components/common/PageLayout/pageLayout.stories.tsx
@@ -1,3 +1,4 @@
+import { FlexBox, Text } from '@cos-ui/react';
 import { ComponentStory, ComponentMeta } from '@storybook/react';
 
 import PageLayout from '.';
@@ -9,5 +10,17 @@ export default {
 } as ComponentMeta<typeof PageLayout>;
 
 export const Default: ComponentStory<typeof PageLayout> = (args) => (
-  <PageLayout {...args} />
+  <PageLayout {...args}>
+    <FlexBox
+      sx={{
+        width: '100%',
+        height: '10rem',
+        bgColor: 'primary_light',
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}
+    >
+      <Text variant="sectionTitle">Page Contents</Text>
+    </FlexBox>
+  </PageLayout>
 );
