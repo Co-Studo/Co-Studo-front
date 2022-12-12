0cd0242c9a97540cd07ce65915411b7a4e2b3590
healtheloper <58503584+healtheloper@users.noreply.github.com>
2022-12-12 15:09:04 +0900

    :sparkles: add more icon

--- src/assets/icons/index.tsx
+++ src/assets/icons/index.tsx
@@ -1,6 +1,7 @@
 import Bell from '@assets/icons/bell.svg';
 import Crown from '@assets/icons/crown.svg';
 import Lock from '@assets/icons/lock.svg';
+import More from '@assets/icons/more.svg';
 import Star from '@assets/icons/star.svg';
 
 export const icon = {
@@ -8,6 +9,7 @@ export const icon = {
   crown: Crown,
   lock: Lock,
   star: Star,
+  more: More,
 };
 
 export type IconName = keyof typeof icon;
