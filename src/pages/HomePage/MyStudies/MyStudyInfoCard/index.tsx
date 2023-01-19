135cbc019be0480389d970011614dec9241ac012
healtheloper <58503584+healtheloper@users.noreply.github.com>
2023-01-19 11:26:20 +0900

    :sparkles: clickable study card

--- src/pages/HomePage/MyStudies/MyStudyInfoCard/index.tsx
+++ src/pages/HomePage/MyStudies/MyStudyInfoCard/index.tsx
@@ -6,6 +6,7 @@ import {
   Paper,
   Text,
 } from '@cos-ui/react';
+import { useNavigate } from 'react-router-dom';
 
 import { StudyEntity } from '@apis/study';
 import Icon from '@components/common/Icon';
@@ -15,74 +16,91 @@ type MyStudyInfoCardProps = {
   study: StudyEntity;
 };
 
-const MyStudyInfoCard = ({ study }: MyStudyInfoCardProps) => (
-  <Paper sx={{ width: '28rem', height: '39rem', padding: '2.2rem' }}>
-    <FlexBox
-      sx={{
-        height: '100%',
-        flexDirection: 'column',
-        justifyContent: 'space-between',
-      }}
+const MyStudyInfoCard = ({ study }: MyStudyInfoCardProps) => {
+  const navigate = useNavigate();
+
+  const handleCardClick = () => {
+    navigate(`/study/${study.id}/introduce`);
+  };
+
+  return (
+    <Paper
+      onClick={handleCardClick}
+      sx={{ width: '28rem', height: '39rem', p: 11 }}
     >
-      <Row
-        title={
-          <FlexBox sx={{ gap: '0.3rem' }}>
-            <Text variant="articleTitle">{study.title}</Text>
-            <Icon iconName="lock" size="small" />
-          </FlexBox>
-        }
-        description={
-          <Text variant="articleDescription" ellipsis={2}>
-            {study.description}
-          </Text>
-        }
-        right={
-          <Icon
-            iconName="star"
-            color={study.isBookmarked ? 'primary' : 'neutral_1'}
-          />
-        }
-        rightProps={{ sx: { alignSelf: 'flex-start' } }}
-      />
-      <FlexBox sx={{ justifyContent: 'space-between' }}>
-        <AvatarGroup>
-          {study.participants.map((user) => (
-            <Avatar key={user.id} src={user.photoURL} alt={user.displayName} />
-          ))}
-        </AvatarGroup>
-        <FlexBox sx={{ flexDirection: 'column', alignItems: 'center' }}>
-          <Icon iconName="crown" />
-          <Avatar
-            size="large"
-            src={study.owner.photoURL}
-            alt={study.owner.displayName}
-          />
-        </FlexBox>
-      </FlexBox>
-      {study.isRequireCheckIn && (
+      <FlexBox
+        sx={{
+          height: '100%',
+          flexDirection: 'column',
+          justifyContent: 'space-between',
+        }}
+      >
         <Row
-          title="체크인"
-          description={`${
-            study.checkInRangeStart ? `${study.checkInRangeStart} ~ ` : ''
-          }${study.checkInRangeEnd}`}
+          title={
+            <FlexBox sx={{ gap: '0.3rem' }}>
+              <Text variant="articleTitle">{study.title}</Text>
+              <Icon iconName="lock" size="small" />
+            </FlexBox>
+          }
+          description={
+            <Text variant="articleDescription" ellipsis={2}>
+              {study.description}
+            </Text>
+          }
+          right={
+            <Icon
+              iconName="star"
+              color={study.isBookmarked ? 'primary' : 'neutral_1'}
+            />
+          }
+          rightProps={{ sx: { alignSelf: 'flex-start' } }}
         />
-      )}
-      {study.isRequireCheckOut && (
-        <Row
-          title="체크아웃"
-          description={`${study.checkOutRangeStart}
+        <FlexBox
+          sx={{ alignItems: 'flex-end', justifyContent: 'space-between' }}
+        >
+          <AvatarGroup>
+            {study.participants.map((user) => (
+              <Avatar
+                key={user.id}
+                src={user.photoURL}
+                alt={user.displayName}
+              />
+            ))}
+          </AvatarGroup>
+          <FlexBox sx={{ flexDirection: 'column', alignItems: 'center' }}>
+            <Icon iconName="crown" />
+            <Avatar
+              size="large"
+              src={study.owner.photoURL}
+              alt={study.owner.displayName}
+            />
+          </FlexBox>
+        </FlexBox>
+        {study.isRequireCheckIn && (
+          <Row
+            title="체크인"
+            description={`${
+              study.checkInRangeStart ? `${study.checkInRangeStart} ~ ` : ''
+            }${study.checkInRangeEnd}`}
+          />
+        )}
+        {study.isRequireCheckOut && (
+          <Row
+            title="체크아웃"
+            description={`${study.checkOutRangeStart}
               ${study.checkOutRangeEnd ? ` ~ ${study.checkOutRangeEnd}` : ''}
               ${study.isCheckOutIsArticle ? ' (글작성 필요)' : ''}`}
-        />
-      )}
-      <FlexBox sx={{ justifyContent: 'space-between', gap: 1 }}>
-        <Button size="medium">체크인</Button>
-        <Button variant="outlined" size="medium">
-          체크아웃
-        </Button>
+          />
+        )}
+        <FlexBox sx={{ justifyContent: 'space-between', gap: 1 }}>
+          <Button size="medium">체크인</Button>
+          <Button variant="outlined" size="medium">
+            체크아웃
+          </Button>
+        </FlexBox>
       </FlexBox>
-    </FlexBox>
-  </Paper>
-);
+    </Paper>
+  );
+};
 
 export default MyStudyInfoCard;
--- src/pages/HomePage/RecruitingStudies/RecrutingStudyCards/RecruitingStudyCard/index.tsx
+++ src/pages/HomePage/RecruitingStudies/RecrutingStudyCards/RecruitingStudyCard/index.tsx
@@ -1,4 +1,5 @@
 import { Avatar, AvatarGroup, FlexBox, Paper, Text } from '@cos-ui/react';
+import { useNavigate } from 'react-router-dom';
 
 import { StudyEntity } from '@apis/study';
 import Row from '@components/common/Row';
@@ -8,36 +9,47 @@ type RecruitingStudyProps = {
   study: StudyEntity;
 };
 
-const RecruitingStudy = ({ study }: RecruitingStudyProps) => (
-  <Paper sx={{ width: '28rem', height: '22.5rem', padding: '2.2rem' }}>
-    <FlexBox
-      sx={{
-        height: '100%',
-        flexDirection: 'column',
-        justifyContent: 'space-between',
-      }}
+const RecruitingStudy = ({ study }: RecruitingStudyProps) => {
+  const navigate = useNavigate();
+
+  const handleCardClick = () => {
+    navigate(`/study/${study.id}/introduce`);
+  };
+
+  return (
+    <Paper
+      onClick={handleCardClick}
+      sx={{ width: '28rem', height: '22.5rem', p: 11 }}
     >
-      <Row
-        title={study.title}
-        description={
-          <Text variant="articleDescription" ellipsis={2}>
-            {study.description}
-          </Text>
-        }
-      />
-      <AvatarGroup>
-        {study.participants.map((user) => (
-          <Avatar
-            key={user.id}
-            size="medium"
-            src={user.photoURL}
-            alt={user.displayName}
-          />
-        ))}
-      </AvatarGroup>
-      <TagList tags={study.tags} />
-    </FlexBox>
-  </Paper>
-);
+      <FlexBox
+        sx={{
+          height: '100%',
+          flexDirection: 'column',
+          justifyContent: 'space-between',
+        }}
+      >
+        <Row
+          title={study.title}
+          description={
+            <Text variant="articleDescription" ellipsis={2}>
+              {study.description}
+            </Text>
+          }
+        />
+        <AvatarGroup>
+          {study.participants.map((user) => (
+            <Avatar
+              key={user.id}
+              size="medium"
+              src={user.photoURL}
+              alt={user.displayName}
+            />
+          ))}
+        </AvatarGroup>
+        <TagList tags={study.tags} />
+      </FlexBox>
+    </Paper>
+  );
+};
 
 export default RecruitingStudy;
